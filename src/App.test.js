8e4ac051855822cf58082a08d981d4e6810da6ce
Joshua Peralta <peralta.joshuapaul07@gmail.com>
2024-07-19 01:21:50 +1200

    update github yml

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Home from "./components/Home";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+test('renders correctly', () => {
+const component = renderer.create(<Home />);
+const tree = component.toJSON();
+expect(tree).toMatchSnapshot();
 });

eb88f493f146d9d95662d2791c85f5e3960af082
Brad Hezh <bradhezh@gmail.com>
2024-07-29 16:21:10 +1200

    about page created with unit and selenium tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Home from "./components/Home";
+import About from "./components/About";
 
 
 
 test('renders correctly', () => {
 const component = renderer.create(<Home />);
+const tree = component.toJSON();
+expect(tree).toMatchSnapshot();
+});
+
+test('renders about page correctly', () => {
+const component = renderer.create(<About />);
 const tree = component.toJSON();
 expect(tree).toMatchSnapshot();
 });
--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 
 function About(props) {
+    const navigate = useNavigate()
+
+    const click = async evt => {
+      navigate('/')
+    }
+
     return (
         <div>
             <h1>Assignment 1</h1>
@@ -8,6 +15,7 @@ function About(props) {
             <p>Developers: Joshua Paul Peralta, Pavitharan Rajaratnam, & Jingzhou Zhang</p>
             <p>Course: SOFT806 CICD</p>
             <p>Project: This is a group code review project.</p>
+            <button onClick={() => click()}>Thank you, Go back to Home page!</button>
         </div>
     );
 }

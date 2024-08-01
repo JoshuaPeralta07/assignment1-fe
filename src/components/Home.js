9d7da46c270a23f9a24373c40d7e839834de1ce0
Joshua Peralta <peralta.joshuapaul07@gmail.com>
2024-08-01 21:58:07 +1200

    Added images and updated home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import dp from '../Images/dp.jpg';
 function Home(props) {
     return (
         <div>
             <h1>Assignment 1</h1>
             <h2>Home page</h2>
+            <img src={dp} alt="dp"/>
         </div>
     );
 }

3ed5274eb3f3c67e37ee4d89d86cc3c10a471b33
Joshua Peralta <peralta.joshuapaul07@gmail.com>
2024-08-04 20:53:54 +1200

    updated homepage and added styling to image

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,9 @@ import dp from '../Images/dp.jpg';
 function Home(props) {
     return (
         <div>
-            <h1>Assignment 1</h1>
-            <h2>Home page</h2>
-            <img src={dp} alt="dp"/>
+            <h1>Welcome to MIS</h1>
+            <img style={{display: "block", margin: "auto", width: 50+"%", paddingTop: 50, border:5,
+            borderStyle: "solid", borderColor: "black"}} src={dp} alt="dp"/>
         </div>
     );
 }
